570b8a3a1ca752eadc1c8beda33eeef8be3874bc
salcedochanza <72934619+salcedochanza@users.noreply.github.com>
2020-11-13 16:37:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica_1___Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World CS Dani Salcedo");
+        }
+    }
+}
